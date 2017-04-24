e620827efcda2b46bca48f6e228a8d1c5485722c
vthglyk <vasgl@intracom-telecom.com>
2017-04-24 07:53:18 +0300

    Modified delete resource to send symbIoTeIds

--- src/main/java/eu/h2020/symbiote/rh/messaging/interworkinginterface/IIFMessageHandler.java
+++ src/main/java/eu/h2020/symbiote/rh/messaging/interworkinginterface/IIFMessageHandler.java
@@ -11,24 +11,33 @@ import javax.annotation.PostConstruct;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import eu.h2020.symbiote.cloud.model.CloudResource;
 import eu.h2020.symbiote.core.model.resources.Resource;
 import eu.h2020.symbiote.core.cci.ResourceRegistryRequest;
 import eu.h2020.symbiote.core.cci.ResourceRegistryResponse;
+import eu.h2020.symbiote.rh.db.ResourceRepository;
+
 import feign.Feign;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
+import java.util.HashMap;
 
 @Component
 public class IIFMessageHandler {
+	
 	private static final Log logger = LogFactory.getLog(IIFMessageHandler.class);
+	
 	private InterworkingInterfaceService jsonclient;
+	
 	@Value("${symbIoTe.interworkinginterface.url}")
 	private String url;
 	
-	
+    @Autowired
+    private ResourceRepository resourceRepository;
+
 	@PostConstruct
 	public void createClient() {
 		logger.info("Will use "+ url +" to access to interworking interface");
@@ -93,6 +102,9 @@ public class IIFMessageHandler {
 
 	public List<String> removeResources(String platformId, List<String> resourceIds)  {
 		ArrayList<String>  result = new ArrayList<String>();
+		ArrayList<String>  debug = new ArrayList<String>();
+		HashMap<String,String> symbioteToInternalIds = new HashMap<String,String>();
+
 		try{
 			logger.info("User trying to removeResources in "+platformId);
 			ArrayList<Resource> listToSend = new ArrayList<Resource>();
@@ -100,8 +112,15 @@ public class IIFMessageHandler {
      	    for (Iterator<String> iter = resourceIds.iterator(); iter.hasNext();){
      	    	String resourceId = (String) iter.next();
      	    	Resource resource = new Resource();
-     	    	resource.setId(resourceId);
-     	    	listToSend.add(resource);
+		        CloudResource existingResource = resourceRepository.getByInternalId(resourceId);
+		        
+		        if (existingResource != null) {
+		    	    resource.setId(existingResource.getResource().getId());
+     	    	    listToSend.add(resource);
+     	    	    debug.add(existingResource.getResource().getId());
+     	    	    symbioteToInternalIds.put(existingResource.getResource().getId(),resourceId);
+		        }
+
      	    } 
 
 			ResourceRegistryRequest request = new ResourceRegistryRequest();
@@ -110,8 +129,9 @@ public class IIFMessageHandler {
 
      	    for (Iterator<Resource> iter = response.getResources().iterator(); iter.hasNext();){
      	    	Resource resource= (Resource) iter.next();
-     	    	result.add(resource.getId());
+	     	    result.add(symbioteToInternalIds.get(resource.getId()));
      	    } 
+
 		}catch(Throwable t){
 			logger.error("Error accessing to AAM server at "+url, t);
 		}
