f067a79434864859e6d2072838ed64d56a48830f
MarioKusek <mario.kusek@fer.hr>
2017-12-21 10:30:29 +0100

    Added arguments to wait for other services.

--- src/main/java/eu/h2020/symbiote/rh/RegistrationHandlerApplication.java
+++ src/main/java/eu/h2020/symbiote/rh/RegistrationHandlerApplication.java
@@ -2,7 +2,7 @@ package eu.h2020.symbiote.rh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**! \class RegistrationHandlerApplication
  * \brief RegistrationHandlerApplication root class that has to be launched to run the RegistrationHandler with spring boot
@@ -15,12 +15,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version: 06/10/2016
 
  */
-//@EnableDiscoveryClient    //when Eureka available
+@EnableDiscoveryClient    //when Eureka available
 //@EnableAutoConfiguration
 @SpringBootApplication
 public class RegistrationHandlerApplication {
 
 	public static void main(String[] args) {
+		WaitForPort.waitForServices(System.getenv("SPRING_BOOT_WAIT_FOR_SERVICES"));
+		
 		SpringApplication.run(RegistrationHandlerApplication.class, args);
     }
 }
